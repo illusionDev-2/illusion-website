9cf84c13fc53d72f08f85fc8a53780279fc4e770
yudachix <yudachix@users.noreply.github.com>
2023-04-29 14:38:04 +0000

    deploy: 20a5ed849ee091dcef45f89ca59092a7650f8330

--- _next/static/chunks/app/layout-aa45e2710c4d0f52.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1282:function(_,n,e){Promise.resolve().then(e.t.bind(e,5779,23))},5779:function(_){_.exports={style:{fontFamily:"'__Noto_Sans_JP_7068cf', '__Noto_Sans_JP_Fallback_7068cf'",fontStyle:"normal"},className:"__className_7068cf"}}},function(_){_.O(0,[604,481,270,744],function(){return _(_.s=1282)}),_N_E=_.O()}]);
