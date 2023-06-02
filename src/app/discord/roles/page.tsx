4eff1a31939774be6f827e9319177396e0b50439
yudachix <73721400+yudachix@users.noreply.github.com>
2023-06-03 05:54:20 +0900

    fix: ロール説明の修正

--- src/app/discord/roles/page.tsx
+++ src/app/discord/roles/page.tsx
@@ -12,26 +12,12 @@ import { fc } from '@/utils/function-component';
 const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME;
 
 const roles = {
-  'TeamAyanamist': '明確な決まりはありませんが、原則創設者にのみ付いているロールです。',
-  'I have Absolute Terror FIELD': dedent`
-    創設者と親しい人のみに付与されるロールです。
-    権限はない見た目だけのロールですが、順位は後述する2ndAyanamistよりも上です。
-  `,
+  'Ayanami': '明確な決まりはありませんが、原則創設者にのみ付いているロールです。',
   '2ndAyanamist': '全権限を持つ管理者のロールです。',
-  'illusionDeveloper': 'プログラミングやサーバー構築など専門的な技術を持つ開発者に与えられるロールです。',
-  'ill-Staff': dedent`
-    一般のモデレーターに当たるロールです。
-    2ndAyanamistより権限は低いですが、タイムアウトやメッセージの削除を行うことができます。
-  `,
-  'illusion premium': dedent`
-    支援者に与えられるロールです。
-    支援者にのみ公開されているプログラムなどを入手できます。
-  `,
   'はまぐりファミリー': dedent`
     2020～2021年ごろに荒らし連合軍というサーバーで活動していた人に与えられるロールです。
     お知らせで発言をする権限がありましたが、現在は削除されています。
-  `,
-  'illusionist': '認証すると手に入るロールです。'
+  `
 } as const satisfies Record<string, string>;
 
 export default fc('Page', () => (
